0710e22a0c7fe1d2178eee1c6986aa3d90446e17
Xrolllex <karolkaczanowski@wp.pl>
2023-11-29 18:59:57 +0100

    commit

---
+++ src/components/Section.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types"
+
+const Section = ({title, children}) => (
+    <section>
+        <h1>{title}</h1>
+        {children}
+    </section>
+
+)
+
+Section.PropTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+}
+
+export default Section
